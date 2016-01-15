ca565005971753ca1130f2156ccad09c9ee3b316
semyons <semyonskvortsov@gmail.com>
2016-01-15 20:50:28 +0700

    проверка пароля
    проверка пароля

--- material/loginer.php
+++ material/loginer.php
@@ -1,13 +1,33 @@
-<?php 
-    $connect= mysql_connect('localhost','semyon','semyon') or die(mysql_error());
-    mysql_select_db('users');
+<?php
+$hostname = "VH237.spaceweb.ru"; 
+$username = "pazdnikova_users"; 
+$password = "adminroot16"; 
+$dbName = "pazdnikova_users"; 
 
-    if (isset($_POST['submit'])){
+
+$userstable = "users"; 
+
+mysql_connect($hostname,$username,$password) OR DIE("Не могу создать соединение "); 
+
+mysql_select_db($dbName) or die(mysql_error());  
+
+
+if (isset($_POST['submit'])){
     	$login = $_POST['login'];
     	$password = $_POST['password'];
-    	$query = mysql_query("SELECT * FROM users_info WHERE login = $login");
-    	if ($_user_data['password']=$password){
-    		header('Location: main.html');}
-    		else {alert ('ne ok');}
+
+    	$query = "SELECT * FROM $userstable";
+
+    	$res = mysql_query($query) or die(mysql_error()); 
+    	$row = mysql_fetch_array($res);
+
+    	if ($row['login'] == $login and $row['password']==$password){
+    		header('Location: main.html');
     	}
+    	else {
+    			echo "not ok";
+    		}
+    }
 ?>
+
+// Проверку для пароля сделай и работает это для root root
